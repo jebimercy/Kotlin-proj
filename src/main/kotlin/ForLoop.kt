13a57d413eb50bff5cfa222bef0f36a86d6512a1
eMobilis <chrispycharmprojects@gmail.com>
2023-03-16 10:39:15 +0300

    first commit

---
+++ src/main/kotlin/ForLoop.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    for(i in 0 .. 50){
+        if (i % 5 == 0){
+            println("$i is divisible")
+        }
+        else{
+            println("$i not divisible")
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>) {
-    
+    var firstName = "Mercy"
+    var lastName = "Precious"
+    println("$firstName $lastName")
 }
