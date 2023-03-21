f4810c5a3938c9b744b3c7183db17d7e0f039c74
eMobilis <chrispycharmprojects@gmail.com>
2023-03-21 10:04:18 +0300

    functions

---
+++ src/main/kotlin/Ex_fuction.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    addnums(num1 = 400, num2 = 400)
+    multynums(num1 = 5, num2 = 400)
+    divdnums(num1 = 100, num2 = 2)
+
+}
+
+fun addnums(num1:Int, num2:Int){
+    var result= num1 + num2
+    println(result)
+}
+
+fun multynums(num1:Int, num2:Int){
+    var result= num1 * num2
+    println(result)
+}
+
+fun divdnums(num1:Int, num2:Int){
+    var result= num1 / num2
+    println(result)
+}
+
+
+
+
---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    sayhello()
+    today()
+}
+
+fun sayhello(){
+    println("Good morning")
+}
+fun  today(){
+    println("Its Tuesday, lets go out")
+}
